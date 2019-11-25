85894f97d5d17fbd5b334991e78a0a3a62bb0fdf
Warya Waxbaro <warya.waxbaro@gmail.com>
2019-11-25 14:41:54 +0200

    add env

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const db = require('../config/key').mongoURI;
+//const db = require('../config/key').mongoURI;
+const db = process.env.DATABASE_URL;
 mongoose.set('debug', true);
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('DB CONNECTED...'))
