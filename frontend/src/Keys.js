5dd6689ec8caf8b6597e31abae92fa470f99e461
Warya Waxbaro <warya.waxbaro@gmail.com>
2019-11-25 15:31:21 +0200

    remove a keys file

--- frontend/src/Keys.js
+++
@@ -1,4 +0,0 @@
-//export const key = 'qa6R69B3GhG6Gy';
-export let key = process.env.REACT_APP_API_KEY;
-export const locationBase = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-export const cityBase = 'http://dataservice.accuweather.com/currentconditions/v1/';
